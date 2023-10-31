59fd26105246ef38b2d8838138483b63155621c2
bruno Pereira <bruno.pereira@sonner.com.br>
2023-10-30 21:08:08 -0300

    First commi, creat project struct

---
+++ src/main/java/com/simpleCrud/demo/SimpleCrudApplication.java
@@ -0,0 +1,13 @@
+package com.simpleCrud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpleCrud/demo/SimpleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleCrud.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
